b9a52ea7f2ac352a996e883dde0233d75a329b0b
Aline Avelino <aline.savelino@gmail.com>
2022-03-14 20:28:34 -0300

    Parte 11 - movimentar inimigo 2 e amigo

--- game1/js/script.js
+++ game1/js/script.js
@@ -30,6 +30,8 @@ function start() {
         movefundo();
         movejogador();
         moveinimigo1();
+        moveinimigo2();
+        moveamigo();
     }
 
     function movefundo() {
@@ -70,6 +72,24 @@ function start() {
             posicaoY = parseInt(Math.random() * 334);
             $("#inimigo1").css("left", 694);
             $("#inimigo1").css("top", posicaoY);
+        }
+    }
+
+    function moveinimigo2() {
+        posicaoX = parseInt($("#inimigo2").css("left"));
+        $("#inimigo2").css("left", posicaoX - 3);
+
+        if (posicaoX <= 0) {
+            $("#inimigo2").css("left", 775);
+        }
+    }
+
+    function moveamigo() {
+        posicaoX = parseInt($("#amigo").css("left"));
+        $("#amigo").css("left", posicaoX + 1);
+
+        if (posicaoX > 906) {
+            $("#amigo").css("left", 0);
         }
     }
 }
