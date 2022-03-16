eb22135813ad0b651d690c8b8d753efc16be9ca0
Aline Avelino <aline.savelino@gmail.com>
2022-03-16 17:46:59 -0300

    Parte 17 - exibir pontuação e quantidade de amigos

--- game1/js/script.js
+++ game1/js/script.js
@@ -5,6 +5,7 @@ function start() {
     $("#fundoGame").append("<div id='inimigo1' class='anima2'></div>");
     $("#fundoGame").append("<div id='inimigo2'></div>");
     $("#fundoGame").append("<div id='amigo' class='anima3'></div>");
+    $("#fundoGame").append("<div id='placar'></div>");
 
     var jogo = {};
     var TECLA = {
@@ -16,6 +17,9 @@ function start() {
     var posicaoY = parseInt(Math.random() * 334);
     var podeAtirar = true;
     var fimdejogo = false;
+    var pontos = 0;
+    var salvos = 0;
+    var perdidos = 0;
 
     jogo.pressionou = [];
     jogo.timer = setInterval(loop, 30);
@@ -35,6 +39,7 @@ function start() {
         moveinimigo2();
         moveamigo();
         colisao();
+        placar();
     }
 
     function movefundo() {
@@ -157,6 +162,8 @@ function start() {
 
         // Disparo com o inimigo1
         if (colisao3.length > 0) {
+            pontos = pontos + 100;
+
             inimigo1X = parseInt($("#inimigo1").css("left"));
             inimigo1Y = parseInt($("#inimigo1").css("top"));
 
@@ -170,6 +177,8 @@ function start() {
 
         // Disparo com o inimigo2
         if (colisao4.length > 0) {
+            pontos = pontos + 50;
+
             inimigo2X = parseInt($("#inimigo2").css("left"));
             inimigo2Y = parseInt($("#inimigo2").css("top"));
             $("#inimigo2").remove();
@@ -182,12 +191,15 @@ function start() {
 
         // jogador com o amigo
         if (colisao5.length > 0) {
+            salvos++;
             reposicionaAmigo();
             $("#amigo").remove();
         }
 
         // Inimigo2 com o amigo
         if (colisao6.length > 0) {
+            perdidos++;
+
             amigoX = parseInt($("#amigo").css("left"));
             amigoY = parseInt($("#amigo").css("top"));
 
@@ -270,5 +282,9 @@ function start() {
                 $("#fundoGame").append("<div id='amigo' class='anima3'></div>");
             }
         }
+    }
+
+    function placar() {
+        $("#placar").html("<h2> Pontos: " + pontos + " Salvos: " + salvos + " Perdidos: " + perdidos + "</h2>");
     }
 }
