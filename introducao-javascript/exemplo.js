81adbef224c2dc524a97ebf93642a79317722c92
Aline Avelino <aline.savelino@gmail.com>
2022-03-22 21:05:09 -0300

    Introdução js - função números pares

---
+++ introducao-javascript/exemplo.js
@@ -0,0 +1,17 @@
+// Recebe um array de números e retorna um array com os números pares e sinaliza quais são os ímpares
+function returnEvenValues(array) {
+    let evenNums = [];
+
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] % 2 === 0) {
+            evenNums.push(array[i]);
+        } else {
+            console.log(`${array[i]} não é par`)
+        }
+    }
+    console.log(`Os números pares são:`, evenNums);
+}
+
+let array = [1, 2, 4, 5, 7, 8, 10];
+
+returnEvenValues(array);
